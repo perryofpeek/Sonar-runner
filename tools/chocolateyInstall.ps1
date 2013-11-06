2d9a2b74a1a553e7f8dd2083a237ad028eb8c5dd
Owain Perry <owain.perry@thetrainline.com>
2013-11-06 19:20:53 +0000

    initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿try {
+  $version = "2.3"	
+  $url = "http://repo1.maven.org/maven2/org/codehaus/sonar/runner/sonar-runner-dist/$version/sonar-runner-dist-$version.zip"
+  $packagename = "sonar-runner-dist-$version"
+  $unzipLocation = "c:\Sonarqube"
+  Install-ChocolateyZipPackage $packageName $url $unzipLocation  
+  Install-ChocolateyPath "$unzipLocation\sonar-runner-$Version\bin" 'Machine'
+  Write-ChocolateySuccess $packagename
+} catch {
+  Write-ChocolateyFailure $packagename $($_.Exception.Message)
+  throw
+}
